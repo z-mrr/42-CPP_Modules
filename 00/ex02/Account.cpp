8fe1589f824d0e60e51cfdcfd6a510eb95fd3fbf
jdias-mo <jdias-mo@student.42porto.com>
2023-01-18 17:05:32 +0000

    test

--- 00/ex02/Account.cpp
+++ 00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 12:04:22 by jdias-mo          #+#    #+#             */
-/*   Updated: 2023/01/18 17:04:04 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:05:20 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,5 @@ _nbDeposits(_totalNbDeposits), _nbWithdrawals(_totalNbWithdrawals)
 {
 	_nbAccounts += 1;
 	_totalAmount += initial_deposit;
-
 }
 
