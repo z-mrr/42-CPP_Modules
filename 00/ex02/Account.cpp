01d4e918413b2b11731cdfb972c2bb1568cb7ede
Jose miguel Dias moreira <jdias-mo@c1r15s13.42porto.com>
2023-01-18 17:01:11 +0000

    asda

--- 00/ex02/Account.cpp
+++ 00/ex02/Account.cpp
@@ -6,7 +6,17 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 12:04:22 by jdias-mo          #+#    #+#             */
-/*   Updated: 2023/01/05 12:04:23 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2023/01/18 16:58:01 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) :
+_accountIndex(_nbAccounts), _amount(_totalAmount),
+_nbDeposits(_totalNbDeposits), _nbWithdrawals(_totalNbWithdrawals)
+{
+	_nbAccounts += 1;
+	_totalAmount += initial_deposit;
+}
 
