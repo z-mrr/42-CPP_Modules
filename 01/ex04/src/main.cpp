fb73d53cefa170591c6616c6d8a9a4bfa8748dd5
zet1r <zet1r@proton.me>
2023-01-22 20:56:18 +0000

    starting cpp01ex04

--- 01/ex04/src/main.cpp
+++ 01/ex04/src/main.cpp
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/22 12:15:34 by jdias-mo          #+#    #+#             */
-/*   Updated: 2023/01/22 20:50:48 by jdias-mo         ###   ########.fr       */
+/*   Created: 2023/01/22 20:55:18 by jdias-mo          #+#    #+#             */
+/*   Updated: 2023/01/22 20:55:42 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanB.hpp"
-#include "Weapon.hpp"
-#include <iostream>
-#include <string>
-
-HumanB::HumanB(std::string name) : _name(name), _weapon(NULL)
-{}
-
-HumanB::~HumanB()
-{}
-
-void	HumanB::attack() const
+int	main()
 {
-	if (_weapon)
-		std::cout << _name << " attacks with their " << _weapon->getType() << '\n';
-}
-
-void	HumanB::setWeapon(Weapon& weapon)
-{
-	_weapon = &weapon;
+	return 0;
 }
