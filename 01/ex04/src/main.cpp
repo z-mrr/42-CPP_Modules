39ddb753a1b4a52eb9e96bd19836d089b136a9a8
jdias-mo <jdias-mo@student.42porto.com>
2023-01-26 19:42:29 +0000

    stating ex04

--- 01/ex04/src/main.cpp
+++ 01/ex04/src/main.cpp
@@ -6,11 +6,18 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 20:55:18 by jdias-mo          #+#    #+#             */
-/*   Updated: 2023/01/22 20:55:42 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2023/01/26 19:28:34 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
+#include <fstream>
+
+int	main(std::string filename, std::string s1, std::string s2)
 {
+	std::ifstream	ifs(filename);
+	std::str
+	std::ofstream	ofs(filename.append("replace"));
+
+
 	return 0;
 }
