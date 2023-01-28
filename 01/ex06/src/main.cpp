2339c161280991b631a6172db409707137f6baa5
jdias-mo <jdias-mo@student.42porto.com>
2023-01-28 13:33:25 +0000

    fix

--- 01/ex06/src/main.cpp
+++ 01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 16:58:33 by jdias-mo          #+#    #+#             */
-/*   Updated: 2023/01/27 22:00:38 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2023/01/28 13:33:12 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 int	main(int ac, char **av)
 {
 	Harl		h;
-	std::string	level(av[1]);
 
 	if (ac != 2)
 		return std::cout << "Usage: ./harlFilter <level>\n", 1;
+	
+	std::string	level(av[1]);
+
 	h.complain(level);
 	return 0;
 }
