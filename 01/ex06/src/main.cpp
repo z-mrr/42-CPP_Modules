917b2e7c829fc63e657753890722d3d201d5c00a
zet1r <zet1r@proton.me>
2023-01-28 19:03:49 +0000

    fix

--- 01/ex06/src/main.cpp
+++ 01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 16:58:33 by jdias-mo          #+#    #+#             */
-/*   Updated: 2023/01/28 13:33:12 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2023/01/28 19:03:32 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int ac, char **av)
 	Harl		h;
 
 	if (ac != 2)
-		return std::cout << "Usage: ./harlFilter <level>\n", 1;
-	
+		return std::cerr << "Usage: ./harlFilter <level>\n", 1;
+
 	std::string	level(av[1]);
 
 	h.complain(level);
